1d9d3115c268823a1df3e12aa0daa5dd661d3b78
martin <martin.alvarado2002@gmail.com>
2024-04-05 13:19:03 +0200

    [31-480]: Using Custom "ng generate" Schematics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainNavComponent } from './main-nav/main-nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MainNavComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-tcg-angular-config';
